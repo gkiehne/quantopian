3a9df34da2123bd79826310335de820bf6778fe6
gkiehne <grant.kiehne@snet.net>
2014-04-25 05:35:08 -0400

    Create l1_median.py

---
+++ l1_median.py
@@ -0,0 +1,4 @@
+# Python implementation of L1-median computation:
+#
+# Vardi, Y. and Zhang, C.-H. (2001). A modified Weiszfeld algorithm 
+# for the Fermat-Weber location problem. Math. Programming, 90, 559-566.
