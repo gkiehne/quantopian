037bc2c4ed3e6c1bbeef3cae7263c491fd249024
Ed Bartosh <bartosh@gmail.com>
2014-04-27 13:48:02 +0300

    Organize docstrings according to PEP08 and PEP257
    Reformatted docstrings according to
    PEP 08 Style Guide for Python Code
    http://legacy.python.org/dev/peps/pep-0008/#documentation-strings
    and
    PEP 257 Docstring Conventions
    http://legacy.python.org/dev/peps/pep-0257/
    
    Added docstrings to all functions.

--- rmr.py
+++ rmr.py
@@ -1,13 +1,23 @@
-# Robust Median Reversion Strategy for On-Line Portfolio Selection,
-# Dingjiang Huang, Junlong Zhou, Bin Li, Steven C.H. Hoi, and Shuigeng
-# Zhou. International Joint Conference on Artificial Intelligence, 2013.
-# http://ijcai.org/papers13/Papers/IJCAI13-296.pdf
+"""
+Robust Median Reversion Strategy for On-Line Portfolio Selection,
+Dingjiang Huang, Junlong Zhou, Bin Li, Steven C.H. Hoi, and Shuigeng
+Zhou. International Joint Conference on Artificial Intelligence, 2013.
+http://ijcai.org/papers13/Papers/IJCAI13-296.pdf
+"""
 
 import numpy as np
 from pytz import timezone
 
 def initialize(context):
+    """
+    Initialize context object.
 
+    Context object is passed to the every call of handle_data.
+    It uses to store data to use in the algo.
+
+    :param context: context object
+    :returns: None
+    """
     context.stocks = [ sid(19662),  # XLY Consumer Discrectionary SPDR Fund
                        sid(19656),  # XLF Financial SPDR Fund
                        sid(19658),  # XLK Technology SPDR Fund
@@ -27,7 +37,17 @@ def initialize(context):
     set_commission(commission.PerTrade(cost=0))
 
 def handle_data(context, data):
+    """
+    The main proccessing function.
 
+    This function is called by quantopian backedn whenever a market event
+    occurs for any of algorithm's specified securities.
+
+    :param context: context object
+    :param data: A dictionary containing market data keyed by security id.
+                 It represents a snapshot of your algorithm's universe as of
+                 when this method was called.
+    """
     prices = history(6,'1d','price').as_matrix(context.stocks)[0:-1,:]
 
     cash = context.portfolio.cash
@@ -84,11 +104,22 @@ def handle_data(context, data):
     rebalance_portfolio(context, b_norm)
 
 def rebalance_portfolio(context, desired_port):
+    """
+    Rebalance portfolio according to desired percentage.
 
+    :param context: context object
+    :param desired_port: list of desired percentages
+    """
     for i, stock in enumerate(context.stocks):
         order_target_percent(stock,desired_port[i])
 
 def intradingwindow_check(context):
+    """
+    Check if algo is in trading window.
+
+    :param context: context object
+    :returns: True if algo is in trading window, False otherwise.
+    """
     # Converts all time-zones into US EST to avoid confusion
     loc_dt = get_datetime().astimezone(timezone('US/Eastern'))
     if loc_dt.hour == 10 and loc_dt.minute > 0:
@@ -97,28 +128,28 @@ def intradingwindow_check(context):
         return False
 
 def simplex_projection(v, b=1):
-    """Projection vectors to the simplex domain
+    """
+    Projection vectors to the simplex domain.
 
-Implemented according to the paper: Efficient projections onto the
-l1-ball for learning in high dimensions, John Duchi, et al. ICML 2008.
-Implementation Time: 2011 June 17 by Bin@libin AT pmail.ntu.edu.sg
-Optimization Problem: min_{w}\| w - v \|_{2}^{2}
-s.t. sum_{i=1}^{m}=z, w_{i}\geq 0
+    Implemented according to the paper: Efficient projections onto the
+    l1-ball for learning in high dimensions, John Duchi, et al. ICML 2008.
+    Implementation Time: 2011 June 17 by Bin@libin AT pmail.ntu.edu.sg
+    Optimization Problem: min_{w}\| w - v \|_{2}^{2}
+    s.t. sum_{i=1}^{m}=z, w_{i}\geq 0
 
-Input: A vector v \in R^{m}, and a scalar z > 0 (default=1)
-Output: Projection vector w
+    Input: A vector v \in R^{m}, and a scalar z > 0 (default=1)
+    Output: Projection vector w
 
-:Example:
->>> proj = simplex_projection([.4 ,.3, -.4, .5])
->>> print proj
-array([ 0.33333333, 0.23333333, 0. , 0.43333333])
->>> print proj.sum()
-1.0
+    :Example:
+    >>> proj = simplex_projection([.4 ,.3, -.4, .5])
+    >>> print proj
+    array([ 0.33333333, 0.23333333, 0. , 0.43333333])
+    >>> print proj.sum()
+    1.0
 
-Original matlab implementation: John Duchi (jduchi@cs.berkeley.edu)
-Python-port: Copyright 2012 by Thomas Wiecki (thomas.wiecki@gmail.com).
-"""
-
+    Original matlab implementation: John Duchi (jduchi@cs.berkeley.edu)
+    Python-port: Copyright 2012 by Thomas Wiecki (thomas.wiecki@gmail.com).
+    """
     v = np.asarray(v)
     p = len(v)
 
