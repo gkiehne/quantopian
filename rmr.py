c2ed761f463a4f5ee8fd14cc6d9ac16f43116e92
Ed Bartosh <bartosh@gmail.com>
2014-05-11 13:06:38 +0300

    Cleanup of l1_median
    Removed trailing whitespaces.
    Fixed docstring indentation.
    
    Signed-off-by: Ed Bartosh <bartosh@gmail.com>

--- rmr.py
+++ rmr.py
@@ -154,23 +154,22 @@ def simplex_projection(v, b=1):
 
 def l1_median(x):
     """
-Computes L1 median (spatial median) using brute force grid search.
+    Computes L1 median (spatial median) using brute force grid search.
 
-:param x: a numpy 1D ndarray (vector) of values
-:returns: scalar estimate of L1 median of values
-"""
-    
+    :param x: a numpy 1D ndarray (vector) of values
+    :returns: scalar estimate of L1 median of values
+    """
     x_min = np.amin(x)
     x_max = np.amax(x)
-    
+
     mu = np.linspace(x_min, x_max, num=50)
-    
+
     sum_dist = np.zeros_like(mu)
-    
+
     for k in range(len(mu)):
         mu_k = mu[k]*np.ones_like(x)
         sum_dist[k] = np.sum(np.absolute(x-mu_k))
-        
+
     l_min = np.argmin(sum_dist)
-    
+
     return mu[l_min]
