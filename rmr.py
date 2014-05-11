4bf77fd419b0de601c909f191df040d31bd6b86f
Ed Bartosh <bartosh@gmail.com>
2014-05-11 14:54:53 +0300

    Reimplement l1_median
    Reimplemented l1_median using scipy.optimize.minimize_scalar
    
    Signed-off-by: Ed Bartosh <bartosh@gmail.com>

--- rmr.py
+++ rmr.py
@@ -7,6 +7,7 @@ http://ijcai.org/papers13/Papers/IJCAI13-296.pdf
 
 import numpy as np
 from pytz import timezone
+from scipy.optimize import minimize_scalar
 
 def initialize(context):
     """
@@ -154,22 +155,25 @@ def simplex_projection(v, b=1):
 
 def l1_median(x):
     """
-    Computes L1 median (spatial median) using brute force grid search.
+    Computes L1 median (spatial median) using scipy.optimize.minimize_scalar
 
     :param x: a numpy 1D ndarray (vector) of values
     :returns: scalar estimate of L1 median of values
     """
-    x_min = np.amin(x)
-    x_max = np.amax(x)
+    x_min = float(np.amin(x))
+    x_max = float(np.amax(x))
 
-    mu = np.linspace(x_min, x_max, num=50)
+    res = minimize_scalar(dist_sum, bounds = (x_min, x_max),
+                          args = tuple(x), method='bounded')
 
-    sum_dist = np.zeros_like(mu)
+    return res.x
 
-    for k in range(len(mu)):
-        mu_k = mu[k]*np.ones_like(x)
-        sum_dist[k] = np.sum(np.absolute(x-mu_k))
+def dist_sum(m, *args):
+    """
+    1D sum of Euclidian distances
 
-    l_min = np.argmin(sum_dist)
-
-    return mu[l_min]
+    :param m: scalar position
+    :param *args: tuple of positions
+    :returns: 1D sum of Euclidian distances
+    """
+    return sum(abs(arg - m) for arg in args)
